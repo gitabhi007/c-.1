3b987e0d63b8e7c88cb5f7c0ccfca31eb05f8ac3
gitabhi007 <68982993+gitabhi007@users.noreply.github.com>
2020-10-29 21:00:23 +0530

    Add files via upload

---
+++ c++.2.cpp
@@ -0,0 +1,14 @@
+//C++ PROGRAM TO FIND THE PRODUCT OF TWO NUMBERS ENTERED BY USER
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a,b,product;
+	cout <<"enter a number here.";
+	cin >>a;
+	cout<<"enter a number here.";
+	cin>>b;
+	product=a*b;
+	cout<<"product="<<product<<endl;
+	return 0;
+}
